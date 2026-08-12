1da6273b85d33a6b8bc211cfe77db1837cbb3f26
XWD-dev <2950412857@qq.com>
2026-08-12 16:18:40 +0800

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import vue from '@vitejs/plugin-vue'
 
 export default defineConfig({
   plugins: [vue()],
-  base: '/GlobalTrade-Shop/',
+  base: '/szhy/',
   server: {
     port: 3000,
     host: '0.0.0.0'
